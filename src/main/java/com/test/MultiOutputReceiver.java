c5c07dabf42df9ba30029f8f2c276b0ae5a47b44
amolgaikwad <amol.gaikwad.31@gmail.com>
2024-07-09 12:46:31 +0530

    Add an example of MultiOutputReceiver

---
+++ src/main/java/com/test/MultiOutputReceiver.java
@@ -0,0 +1,58 @@
+package com.test;
+
+import com.test.transforms.Transforms;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.sdk.values.TupleTagList;
+
+@Slf4j
+public class MultiOutputReceiver {
+
+  public static void main(String[] args) {
+    PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
+    Pipeline pipeline = Pipeline.create(options);
+
+    PCollection<Integer> numbers = pipeline.apply("Numbers", Create.of(10, 50, 120, 20, 200, 0));
+
+    TupleTag<Integer> numbersBelow100 = new TupleTag<>() {
+    };
+    TupleTag<Integer> numbersAbove100 = new TupleTag<>() {
+    };
+
+    PCollectionTuple pCollectionTuple = applyTransform(numbers, numbersBelow100, numbersAbove100);
+    pCollectionTuple.get(numbersBelow100).apply("LogNumbers < 100", ParDo.of(new Transforms.LogIntegers("Number < 100")));
+    pCollectionTuple.get(numbersAbove100).apply("LogNumbers > 100", ParDo.of(new Transforms.LogIntegers("Number > 100")));
+
+
+    pipeline.run().waitUntilFinish();
+
+  }
+
+  static PCollectionTuple applyTransform(PCollection<Integer> input,
+                                         TupleTag<Integer> numBelow100Tag,
+                                         TupleTag<Integer> numAbove100Tag) {
+
+    return input.apply(ParDo.of(new DoFn<Integer, Integer>() {
+
+      @ProcessElement
+      public void processElement(@Element Integer number, MultiOutputReceiver out) {
+        if (number <= 100) {
+          // First PCollection
+          out.get(numBelow100Tag).output(number);
+        } else {
+          // Additional PCollection
+          out.get(numAbove100Tag).output(number);
+        }
+      }
+
+    }).withOutputTags(numBelow100Tag, TupleTagList.of(numAbove100Tag)));
+  }
+}
--- src/main/java/com/test/transforms/Transforms.java
+++ src/main/java/com/test/transforms/Transforms.java
@@ -15,9 +15,20 @@ public class Transforms {
   }
 
   public static class LogIntegers extends DoFn<Integer, Integer> {
+
+    private final String prefix;
+
+    public LogIntegers() {
+      this.prefix = "Processing element";
+    }
+
+    public LogIntegers(String prefix) {
+      this.prefix = prefix;
+    }
+
     @ProcessElement
     public void processElement(ProcessContext c) {
-      log.info("Processing Integer: {}", c.element());
+      log.info(" {}: {}", prefix, c.element());
       c.output(c.element());
     }
   }
